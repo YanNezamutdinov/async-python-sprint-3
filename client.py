8d73d56e6cc003e018d8233f1ce1eb349484b19d
YanNezamutdinov <nezamutdinovyan@gmail.com>
2022-11-07 19:17:25 +0300

    init

--- client.py
+++ client.py
@@ -1,10 +1,31 @@
-# import asyncio
-# import aiohttp
+import asyncio
+import json
 
 
-class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+async def client(message: str, user: str = "Anonymous"):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8000)
+    query = {'user': user,
+             'message': message}
 
-    def send(self, message=""):
-        pass
+    # print(f'Send: {message!r}')
+    writer.write(json.dumps(query).encode())
+    await writer.drain()
+
+    data = await reader.read(100)
+    # print(f'Received: {data.decode()!r}')
+
+    # print('Close the connection')
+    writer.close()
+    await writer.wait_closed()
+
+asyncio.run(client("Say HI!"))
+
+
+
+# class Client:
+#     def __init__(self, server_host="127.0.0.1", server_port=8000):
+#         pass
+#
+#     def send(self, message=""):
+#         pass
--- server.py
+++ server.py
@@ -1,9 +1,37 @@
-# import asyncio
+import asyncio
+import json
+import sys
+from asyncio import StreamReader, StreamWriter
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# logger.info('Incoming data: %s', message)
+
+async def client_connected(reader: StreamReader, writer: StreamWriter):
+    # address = writer.get_extra_info('peername')
+
+    while True:
+        data = await reader.read(1024)
+        print(data)
+        message = json.loads(data.decode())
+        if not message:
+            break
+        message_for_client = {'test': 'test'}
+        data_for_client = json.dumps(message_for_client).encode()
+        writer.write(data_for_client)
+        await writer.drain()
+    writer.close()
 
 
-class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
-        pass
+async def server(host: str = "127.0.0.1", port: int = 8000):
+    srv = await asyncio.start_server(
+        client_connected, host, port)
 
-    def listen(self):
-        pass
+    async with srv:
+        await srv.serve_forever()
+
+asyncio.run(server())
+
