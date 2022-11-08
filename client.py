023889956dc36c594e98ecf1265879fe44ce6084
YanNezamutdinov <nezamutdinovyan@gmail.com>
2022-11-08 17:04:33 +0300

    1

--- client.py
+++ client.py
@@ -2,26 +2,41 @@ import asyncio
 import json
 
 
-async def client(message: str, user: str = "Anonymous"):
+async def client(message: str = None, user: str = "Anonymous"):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8000)
     query = {'user': user,
              'message': message}
-
-    # print(f'Send: {message!r}')
-    writer.write(json.dumps(query).encode())
+    data_for_client = json.dumps(query).encode()
+    writer.write(data_for_client)
     await writer.drain()
 
-    data = await reader.read(100)
-    # print(f'Received: {data.decode()!r}')
+    data = await reader.read(1024)
+    print(data.decode())
+    print(type(json.loads(data.decode())))
 
-    # print('Close the connection')
+    # message = )
+    # print(message)
+    # for record in message:
+    #     if not record:
+    #         print("В чате пока пусто")
+    #     else:
+    #         print(record)
+    # print(f'Received: {data.decode()!r}')
     writer.close()
     await writer.wait_closed()
 
-asyncio.run(client("Say HI!"))
+asyncio.run(client(user="Agent Smith"))
+asyncio.run(client(message="Hi Neo!", user="Agent Smith"))
+asyncio.run(client(message="I'm mister Smith", user="Agent Smith"))
+asyncio.run(client(message="I see you!", user="Agent Smith"))
+asyncio.run(client(message="...", user="Agent Smith"))
+asyncio.run(client(message="...look back Neo...", user="Agent Smith"))
 
 
+
+# ―Вы слышите, мистер Андерсон? Это рок, неизбежность. Шаги Вашей смерти. Прощайте, мистер Андерсон.
+# ―Меня зовут Нео.
 
 # class Client:
 #     def __init__(self, server_host="127.0.0.1", server_port=8000):
--- server.py
+++ server.py
@@ -1,28 +1,56 @@
 import asyncio
 import json
 import sys
+import uuid
 from asyncio import StreamReader, StreamWriter
 import logging
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # logger.info('Incoming data: %s', message)
+open("general_chat.json", 'w+')
+with open("general_chat.json", 'r') as f:
+    try:
+        general_chat = json.load(f)
+    except ValueError:
+        general_chat = dict()
+
 
 async def client_connected(reader: StreamReader, writer: StreamWriter):
     # address = writer.get_extra_info('peername')
 
     while True:
         data = await reader.read(1024)
-        print(data)
-        message = json.loads(data.decode())
-        if not message:
+        if not data:
             break
-        message_for_client = {'test': 'test'}
-        data_for_client = json.dumps(message_for_client).encode()
-        writer.write(data_for_client)
-        await writer.drain()
+
+        message = json.loads(data.decode())
+        if not message.get('message'):
+            data_for_client = json.dumps(general_chat).encode()
+            writer.write(data_for_client)
+            await writer.drain()
+        elif not message.get('to_user'):
+            record = {str(uuid.uuid4()): {'user': message.get('user'), 'message': message.get('message')}}
+            general_chat.update(record)
+            data_for_client = json.dumps(record).encode()
+            writer.write(data_for_client)
+            await writer.drain()
+            with open("general_chat.json", 'w') as f:
+                json.dump(general_chat, f)
+        else:
+            # p2p
+            pass
+
+        # with open("general_chat.json", 'w') as f:
+        #     json.dump(general_chat, f)
+        #
+        # data_for_client = json.dumps(general_chat).encode()
+        # writer.write(data_for_client)
+        # await writer.drain()
+
     writer.close()
 
 
