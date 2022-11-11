393082a6460c7468d06c1cb807a99b5efb601943
YanNezamutdinov <nezamutdinovyan@gmail.com>
2022-11-11 18:35:24 +0300

    4

--- server.py
+++ server.py
@@ -10,20 +10,14 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # logger.info('Incoming data: %s', message)
-open("general_chat.json", 'w')
+open("chats.json", 'w')
 open("last_read_message.json", 'w')
-
+open("chat_name.json", 'w')
 
 
 async def client_connected(reader: StreamReader, writer: StreamWriter):
-    # address = writer.get_extra_info('peername')
-    # print(address)
 
-    with open("general_chat.json", 'r') as f:
-        try:
-            general_chat = json.load(f)
-        except ValueError:
-            general_chat = list()
+
 
     while True:
         data = await reader.read(1024)
@@ -31,32 +25,46 @@ async def client_connected(reader: StreamReader, writer: StreamWriter):
             break
 
         message = json.loads(data.decode())
-        _user, _message, _last_read_message, _to_user = message.values()
+        _user, _renew, _message, _to_user = message.values()
 
-        if _last_read_message and not _message:
-            index_last_message = \
-            [index for index, item in enumerate(general_chat) if item[0] == _last_read_message][0]
-            no_read_message = general_chat[index_last_message+1:]
-            data_for_client = json.dumps(no_read_message).encode()
-            writer.write(data_for_client)
-            await writer.drain()
+        if _renew:
+            with open("chat_name.json", 'r') as f:
+                try:
+                    chat_name = json.load(f)
+                except ValueError:
+                    chat_name = dict()
 
-        elif not _message:
-            data_for_client = json.dumps(general_chat[-20:]).encode()
-            writer.write(data_for_client)
-            await writer.drain()
+            chat_ids = [key for key, values in chat_name if _user in values]
 
-        elif not message.get('to_user'):
-            rec = [str(uuid.uuid4()), _user, _message]
-            general_chat.append(rec)
-            data_for_client = json.dumps([rec,]).encode()
-            writer.write(data_for_client)
-            await writer.drain()
-            with open("general_chat.json", 'w') as f:
-                json.dump(general_chat, f)
+            if chat_ids:
+                for id in chat_ids:
+
+
+        # if _last_read_message and not _message:
+        #     index_last_message = \
+        #     [index for index, item in enumerate(general_chat) if item[0] == _last_read_message][0]
+        #     no_read_message = general_chat[index_last_message+1:]
+        #     data_for_client = json.dumps(no_read_message).encode()
+        #     writer.write(data_for_client)
+        #     await writer.drain()
+        #
+        # elif not _message:
+        #     data_for_client = json.dumps(general_chat[-20:]).encode()
+        #     writer.write(data_for_client)
+        #     await writer.drain()
+        #
+        # elif not message.get('to_user'):
+        #     rec = [str(uuid.uuid4()), _user, _message]
+        #     general_chat.append(rec)
+        #     data_for_client = json.dumps([rec,]).encode()
+        #     writer.write(data_for_client)
+        #     await writer.drain()
+        #     with open("general_chat.json", 'w') as f:
+        #         json.dump(general_chat, f)
         # else:
             # p2p
             # pass
+
     writer.close()
 
 
